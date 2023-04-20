0384f2416e8af25f3839719ff56152ed7c8b2e38
Neowaps <Neowapsn1@mail.ru>
2023-04-20 10:26:59 +0300

    complete Task19

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿System.Console.Write("Введите пятизначное число: ");
+string number = Console.ReadLine();
+
+if (number.Length != 5)
+{
+    System.Console.WriteLine("Вы ввели не пятизначное число!");
+}
+else
+    if (number[0] == number[4] && number[1] == number[3])
+    {
+        System.Console.WriteLine("Да, число является палиндромом");
+    }
+    else
+    {
+        System.Console.WriteLine("Нет, число не является палиндромом");
+    }
